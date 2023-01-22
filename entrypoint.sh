7576529d119bcb6b26bad85da8f14804898cd1f8
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-01-22 20:49:36 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@
 
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/v2ray/config.json
 sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
+bash <(curl -fsSL git.io/warp.sh) proxy
 exec "$@"
